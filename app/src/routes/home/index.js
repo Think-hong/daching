a9d250a7e3f8cec2a6d367ae92fa34fa3dc2054f
hong seo yun <thinkhong99@naver.com>
2022-01-28 19:09:04 +0900

    이제 시작

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
-router.get("/register", ctrl.output.register);
+/*router.get("/register", ctrl.output.register);
 router.get("/login", ctrl.output.login);
 router.post("/login", ctrl.deliver.login);
-router.post("/register", ctrl.deliver.register);
+router.post("/register", ctrl.deliver.register);*/
 
 
 module.exports = router;
